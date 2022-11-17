e73b5dfd0108d683936dce72f1fd046c303b291d
Abigail Ross <abigailross@Abigails-MacBook-Pro-2.local>
2022-11-17 13:49:21 +0000

    Introduction of web socket basics

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
 import express from 'express'
-const app = express();
 
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
@@ -11,9 +10,16 @@ import messageHistoryRouter from './src/routes/messages/messageHistoryAPI.js';
 import userRouter from './src/routes/userAPI.js';
 import messagesRouter from './src/routes/messages/messageListAPI.js';
 
-dotenv.config();
+import { createServer } from 'http';
+import { Server } from 'socket.io';
 
 const port = 5001;
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+dotenv.config();
 
 // Connect to the database
 mongoose
@@ -35,7 +41,28 @@ app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.use(bodyParser.json());
+//websocket
+
+const httpServer = createServer(app);
+const io = new Server(httpServer, { cors: { origin: '*' } });
+
+io.on('connection', function(socket){
+  console.log('User is connected');
+
+  socket.on('disconnect', function(){
+    console.log('User has been disconnected');
+  });
+
+  socket.on('toServer', function(){
+    console.log('all the way from the API!');
+  });
+
+  socket.emit('toAPI')
+});
+
+io.listen(8000);
+
+//routes
 
 app.use('/user', userRouter);
 
